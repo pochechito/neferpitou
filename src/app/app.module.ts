1b6137acb04763f827193ade74de8edc2d53fde6
Sergio Posada Urrea <sergio.posadaurrea@gmail.com>
2020-04-14 22:13:53 -0500

    Blank

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { ButterflyButtonModule } from "magic-button-pochechito/projects/butterfly-button/src/public-api";
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ButterflyButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
